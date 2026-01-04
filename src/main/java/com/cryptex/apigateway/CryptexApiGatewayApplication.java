22c520f0b55c68e5e80f41ef3cc61e9c8088b0be
Akansha Roy <akansharoy2008@gmail.com>
2026-01-04 23:08:48 +0530

    initial commit

---
+++ src/main/java/com/cryptex/apigateway/CryptexApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cryptex.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptexApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptexApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptex/apigateway/CryptexApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptex.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptexApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
